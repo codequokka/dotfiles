952767c6997b39736e78d284ff8c5dc482f03bb9
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-01 08:26:35 +0900

    Install hub into mac

--- setup/mac/setup.sh
+++ setup/mac/setup.sh
@@ -5,3 +5,6 @@ ${HOME}/dotfiles/setup/mac/install_cmdlinetools.sh
 
 # Install homebrew
 ${HOME}/dotfiles/setup/mac/install_homebrew.sh
+
+# Install apps by using homebrew
+${HOME}/dotfiles/setup/mac/brew.sh
