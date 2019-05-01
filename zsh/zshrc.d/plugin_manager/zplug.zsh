b8741709ddc7b8f97004c2cf86a2e321230ac8e9
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-01 10:58:55 +0900

    Install zplug into zsh

---
+++ zsh/zshrc.d/plugin_manager/zplug.zsh
@@ -0,0 +1,13 @@
+export ZPLUG_HOME=${HOME}/.zplug
+source ${ZPLUG_HOME}/init.zsh
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf 'Install? [y/N]: '
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load
--- zshrc
+++ zshrc
@@ -1,3 +1,6 @@
+# Plugin manager
+source ~/.zsh/zshrc.d/plugin_manager/zplug.zsh
+
 # Plugins
 for zshrc in ~/.zsh/zshrc.d/plugin/*.zsh; do
     source $zshrc
