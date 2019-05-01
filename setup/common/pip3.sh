a5c246700e9c5e6c263f4c4f49869afa8df7fa0d
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-01 17:17:03 +0900

    Install pipenv into system python3

---
+++ setup/common/pip3.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Venv
+pip3 install pipenv
--- setup/common/setup.sh
+++ setup/common/setup.sh
@@ -2,3 +2,6 @@
 
 # Make likns from dotfiles/ to ${HOME}/
 ${HOME}/dotfiles/setup/common/make_links.sh
+
+# Install packages for system python3
+${HOME}/dotfiles/setup/common/pip3.sh
