f32e6b0863473c0dc7163344bfd2fbee0876caf4
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-01 11:18:19 +0900

    Install enhancd into zsh

--- zsh/zshrc.d/plugin_manager/zplug.zsh
+++ zsh/zshrc.d/plugin_manager/zplug.zsh
@@ -1,6 +1,9 @@
 export ZPLUG_HOME=${HOME}/.zplug
 source ${ZPLUG_HOME}/init.zsh
 
+# Cd
+zplug "b4b4r07/enhancd", use:init.sh
+
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
     printf 'Install? [y/N]: '
