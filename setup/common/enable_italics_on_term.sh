def2e6c9ab13ebb7c17542378cf663623692f18c
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-04 11:06:33 +0900

    Enable true color on tmux, iterm2

---
+++ setup/common/enable_italics_on_term.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+tic ${HOME}/dotfiles/setup/common/xterm-256color-italic.terminfo
--- setup/common/setup.sh
+++ setup/common/setup.sh
@@ -5,3 +5,6 @@ ${HOME}/dotfiles/setup/common/make_links.sh
 
 # Install packages for system python3
 ${HOME}/dotfiles/setup/common/pip3.sh
+
+# Enable italic font on terminal
+${HOME}/dotfiles/setup/common/enable_italics_on_term.sh
