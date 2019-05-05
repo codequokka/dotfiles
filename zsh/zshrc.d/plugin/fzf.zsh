753744b731043806f1629b6c0e72214d407ac195
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-06 06:02:37 +0900

    Add option to preview file on fzf

--- zsh/zshrc.d/plugin/fzf.zsh
+++ zsh/zshrc.d/plugin/fzf.zsh
@@ -1,3 +1,4 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
