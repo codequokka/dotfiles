d02b2b0a985d10b74cbafd5d9fc2a69eecc02508
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-11 18:32:04 +0900

    Install zsh-syntax-highlighting into zsh

--- zsh/zshrc.d/plugin_manager/zplug.zsh
+++ zsh/zshrc.d/plugin_manager/zplug.zsh
@@ -13,6 +13,9 @@ zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
 zplug "docker/cli", use:"contrib/completion/zsh/_docker", lazy:true
 
+# Syntax highlighting
+zplug "zsh-users/zsh-syntax-highlighting"
+
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
     printf 'Install? [y/N]: '
