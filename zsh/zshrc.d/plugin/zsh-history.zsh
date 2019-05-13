7aefff170701aabab1cbdcefc046d7821d103c4e
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-13 16:46:16 +0900

    Update zsh-history settings

--- zsh/zshrc.d/plugin/zsh-history.zsh
+++ zsh/zshrc.d/plugin/zsh-history.zsh
@@ -1,3 +1,5 @@
+export PATH=${HOME}/dotfiles/bin:$PATH
+
 # DB file path
 export ZSH_HISTORY_FILE="$HOME/.zsh_history.db"
 # CLI selector
@@ -5,8 +7,9 @@ export ZSH_HISTORY_FILTER="fzy:fzf:peco:percol"
 
 # History per directory
 # export ZSH_HISTORY_KEYBIND_GET_BY_DIR="^r"
+
 # All histories
-# export ZSH_HISTORY_KEYBIND_GET_ALL="^x^x"
+export ZSH_HISTORY_KEYBIND_GET_ALL="^s^s"
 
 # Run any SQLs on original selector I/F (with screen)
 # export ZSH_HISTORY_KEYBIND_SCREEN="^r^r"
