6b96eadcb1e98057063072b1deea29f59c12f802
Nobuchika Tanaka <codequokka@gmail.com>
2019-06-02 04:03:54 +0900

    Add alias settings for zsh

---
+++ zsh/zshrc.d/zsh/alias.zsh
@@ -0,0 +1,18 @@
+# Ls
+alias l='colorls -lA --sd'
+alias la='ls -laG'
+alias lat='ls -latG'
+alias ls='colorls -lA --sd'
+
+# Nvim
+alias n='nvim'
+
+# Git
+alias g='git'
+
+# Cd
+alias c='cd'
+alias cgr='cd-gitroot'
+
+# Bat
+alias b='bat'
